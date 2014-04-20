ca70c5d89abd53afaba122fd727fb05854bcb912
Luke Oatham <lukeoatham@gmail.com>
2014-04-21 00:53:30 +0100

    Staff directory masonry template
    Signed-off-by: Luke Oatham <lukeoatham@gmail.com>

--- govintranetpress/page-staff-directory-masonry.php
+++ govintranetpress/page-staff-directory-masonry.php
@@ -1,11 +1,16 @@
 <?php
-/* Template name: Staff directory masonry*/
-	 wp_register_script( 'masonry.pkgd.min', get_stylesheet_directory_uri() . "/js/masonry.pkgd.min.js");
-	 wp_enqueue_script( 'masonry.pkgd.min',95 );
+/* Template name: Staff directory grid*/
 					
 get_header(); ?>
 
 <?php 
+
+	 wp_register_script( 'masonry.pkgd.min', get_stylesheet_directory_uri() . "/js/masonry.pkgd.min.js");
+	 wp_enqueue_script( 'masonry.pkgd.min',95 );
+
+	 wp_register_script( 'imagesloaded.pkgd.min', get_stylesheet_directory_uri() . "/js/imagesloaded.pkgd.min.js");
+	 wp_enqueue_script( 'imagesloaded.pkgd.min',94 );
+
 
 $fulldetails=get_option('general_intranet_full_detail_staff_cards'); // 1 = show
 $directorystyle = get_option('general_intranet_staff_directory_style'); // 0 = squares, 1 = circles
@@ -21,63 +26,90 @@ $grade = $_GET["grade"];
 
 if ( have_posts() ) while ( have_posts() ) : the_post(); 
 ?>
-<div class="row">
-	<div class="col-lg-8 col-md-8 col-sm-8">
-		<div class="breadcrumbs">
-			<a href="<?php echo site_url(); ?>">Home</a>
-			&raquo; <?php the_title(); ?>
-		</div>
-		<div class="col-lg-12">
-		<h1><?php the_title(); ?></h1>
-		<form class="form-horizontal" role="form" id="searchform2" name="searchform2" action="<?php echo home_url( '/search-staff/' ); ?>">
-	
-		  <div class="col-lg-12">
-			<div id="staff-search" class="well well-sm">
-					<div class="input-group">
-				    	 <input type="text" class="form-control" placeholder="Search for a name, job title, skills, phone number..." name="q" id="s2" value="<?php echo $_GET['s'];?>">
-						 <span class="input-group-btn">
-						 <button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-						 </span>
-					</div><!-- /input-group -->
-				  </div>
+	<div class="row">
+		<div class="col-lg-8 col-md-8 col-sm-8">
+			<div class="breadcrumbs">
+				<a href="<?php echo site_url(); ?>">Home</a>
+				&raquo; <?php the_title(); ?>
 			</div>
-		</form>
+			<div class="col-lg-12 col-md-12 col-sm-12">
+				<h1><?php the_title(); ?></h1>
+			</div>
+		<div>
+		<form class="form-horizontal" role="form" id="searchform2" name="searchform2" action="<?php echo site_url( '/search-staff/' ); ?>">
+
+	  <div class="col-lg-12 col-md-12 col-sm-12 ">
+		<div id="staff-search" class="well well-sm">
+				<div class="input-group">
+			    	 <input type="text" class="form-control pull-left" placeholder="Name, job title, skills, team, number..." name="q" id="s2">
+					 <span class="input-group-btn">
+						 <button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+					 </span>
+					 
+				
+				<?php
+					
+			  	$terms = get_terms('team',array('hide_empty'=>false,'parent' => '0',));
+				if ($terms) {
+					$otherteams='';
+			  		foreach ((array)$terms as $taxonomy ) {
+			  		    $themeid = $taxonomy->term_id;
+			  		    $themeURL= $taxonomy->slug;
+			  			$otherteams.= " <li><a href='".site_url()."/team/{$themeURL}/'>".govintranetpress_custom_title($taxonomy->name)."</a></li>";
+			  		}  
+			  		
+			  		$teamdrop = get_option('general_intranet_team_dropdown_name');
+			  		if ($teamdrop=='') $teamdrop = "Browse teams";
+			  		echo "<div class='btn-group pull-right'><button type='button' class='btn btn-info dropdown-toggle4' data-toggle='dropdown'>".$teamdrop." <span class='caret'></span></button><ul class='dropdown-menu' role='menu'>".$otherteams."</ul></div><div class='btn-group pull-right'><button class='btn btn-link disabled'>Or&nbsp;</button></div>";
+				}
+
+				?>
+				</div><!-- /input-group -->
+				
+			  </div>
 		</div>
-	</div>	
-<script>
-jQuery("#s2").focus();
-</script>							
-
-	<div class="col-lg-4 col-md-4">
-	<!-- intentionally left blank -->
+	</form>		</div><!--end-->	
+		<script>
+		jQuery("#s2").focus();
+		</script>							
+	
+		<div class="col-lg-4 col-md-4 col-sm-4">
+		<!-- intentionally left blank -->
+		</div>
 	</div>
-
-<div class="col-lg-12">
-<?php
+	<div class="col-lg-12 col-md-12 col-sm-12">
+<?php 
 			if ($sort == 'last' || $sort == 'first'){
 
 				$letters = range('A','Z');
+
+				$activeletter = ($_REQUEST['show']);
+				if (!$activeletter) $activeletter = "A";
 				
 				foreach($letters as $l) {
 					
-					$letterlink[$l] = "<li class='disabled {$l}'><a>".$l."</a></li>";
+					$hasentries[$l] = $hasentries[$l] + 1;				
+					if ($l == $activeletter) {
+						$letterlink[$l] = "<li  class='{$l} active'><a href='?grade=".$grade."&amp;show=".$l."&amp;sort={$sort}'>".$l."</a></li>";
+					} else {
+						$letterlink[$l] = "<li  class='{$l}'><a href='?grade=".$grade."&amp;show=".$l."&amp;sort={$sort}'>".$l."</a></li>";
+					}						
 				}				
 				
 				?>	
 
 
-			<div class="col-lg-12">
-			<?php the_content(); ?>
+			<div class="col-lg-12 col-md-12 col-sm-12">
 					<ul id='atozlist' class="pagination">
 					
 					<?php
 					if ($sort == 'last'){
-						$q = "select user_id, meta_value as name from wp_usermeta where meta_key = 'last_name' order by meta_value asc";
+						$q = "select user_id, meta_value as name from wp_usermeta where meta_key = 'last_name' and ucase(left(meta_value,1)) = '".strtoupper($_REQUEST['show'])."' order by meta_value asc";
 					} elseif ($sort == "first"){
-						$q = "select user_id, meta_value as name from wp_usermeta where meta_key = 'first_name' order by meta_value asc";
+						$q = "select user_id, meta_value as name from wp_usermeta where meta_key = 'first_name' and ucase(left(meta_value,1)) = '".strtoupper($_REQUEST['show'])."' order by meta_value asc";
 					}					
 					$userq = $wpdb->get_results($q,ARRAY_A);
-
+					$html="<div class='col-lg-12 col-md-12 col-sm-12'>";
 					foreach ($userq as $u){ 
 						$usergrade = get_user_meta($u['user_id'],'user_grade',true); 
 						$title = $u['name'];
@@ -101,31 +133,49 @@ jQuery("#s2").focus();
 								if ($gradecode && $showgrade){
 									$gradedisplay = "<span class='badge pull-right'>".$gradecode."</span>";
 								}
-							if ($directorystyle==1){
-							$avatarhtml = str_replace('avatar-66', 'avatar-66 pull-left indexcard-avatar img img-responsive img-circle', get_avatar($u['user_id'],66));
-							} else {
-							$avatarhtml = str_replace('avatar-66', 'avatar-66 pull-left indexcard-avatar img img-responsive ', get_avatar($u['user_id'],66));
-							}
+							 	if (function_exists('get_wp_user_avatar_src')){	
+										$imgsrc = get_wp_user_avatar_src($userid,'thumbnail');				
+										if ($directorystyle==1){
+											$avatarhtml = "<img class='img img-circle alignleft' src='".$imgsrc."' width='66'  height='66' alt='".$displayname."' />";
+										}else{
+											$avatarhtml = "<img class='img alignleft' src='".$imgsrc."' width='66'  height='66' alt='".$displayname."' />";
+										}
+								} else {
+									$avatarhtml = get_avatar($userid,66);
+									$avatarhtml = str_replace("photo", "photo alignleft", $avatarhtml);
+								}
 							if ($fulldetails){
-								$html .= "<div class='grid-item'><div class='media well well-sm'><a href='".site_url()."/staff/".$user_info->user_nicename."/'>".$avatarhtml."</a><div class='media-body'><p><a href='".site_url()."/staff/".$user_info->user_nicename."/'><strong>".$displayname."</strong>".$gradedisplay."</a><br>";
+								$html .= "<div class='pgrid-item'><div class='media well well-sm'><a href='".site_url()."/staff/".$user_info->user_nicename."/'>".$avatarhtml."</a><div class='media-body'><p><a href='".site_url()."/staff/".$user_info->user_nicename."/'><strong>".$displayname."</strong>".$gradedisplay."</a><br>";
 
 								// display team name(s)
 								$poduser = new Pod ('user' , $userid);
 								$terms = $poduser->get_field('user_team');
 								if ($terms) {				
-									$teamlist = array();
-							  		foreach ($terms as $taxonomy ) {
-							  			$teamlist[]= $taxonomy['name'];
-							  			$html.= implode(" &raquo; ", $teamlist)."<br>";
-									}
+									foreach ($terms as $taxonomy ) { //print_r($taxonomy);
+							  		    $themeid = $taxonomy['term_id'];
+							  		    $themeURL= $taxonomy['slug'];
+							  		    $themeparent = $taxonomy['parent']; 
+							   		    if ($themeURL == 'uncategorized') {
+								  		    continue;
+							  		    }
+							  		    while ($themeparent!=0){
+							  		    	$parentteam = get_term_by('id', $themeparent, 'team'); 
+								  			$themeparent = $parentname->parent; 
+							  		    }
+							  		    
+							  		    $parentslug = $parentteam->slug;
+							  			$teamlist= govintranetpress_custom_title($parentteam->name);
+							  			$html.= "".$teamlist."<br>";
+			
+							  		}
 								}  
 								
 								?>
 
-							<?php if ( get_user_meta($userid ,'user_job_title',true )) : 
-				
-								$html .= ''.get_user_meta($userid ,'user_job_title',true )."<br>";
-				
+						<?php if ( get_user_meta($userid ,'user_job_title',true )) : 
+									$meta = get_user_meta($userid ,'user_job_title',true );
+									$meta = str_replace(" ", "&nbsp;", $meta);
+									$html.=$meta."<br>";
 							endif; ?>
 	
 							
@@ -146,56 +196,80 @@ jQuery("#s2").focus();
 								$counter++;
 								
 							} else {
-							if ($directorystyle==1){
-							$avatarhtml = str_replace('avatar-66', 'avatar-66 pull-left indexcard-avatar img img-responsive img-circle', get_avatar($u['user_id'],66));
-							} else {
-							$avatarhtml = str_replace('avatar-66', 'avatar-66 pull-left indexcard-avatar img img-responsive ', get_avatar($u['user_id'],66));
-								
-							}
-							
-								$html .= "<div class='grid-item'><div class='indexcard'><a href='".site_url()."/staff/".$user_info->user_nicename."/'><div class='media'>".$avatarhtml."<div class='media-body'><strong>".$displayname."</strong>".$gradedisplay."<br>";
+							 	if (function_exists('get_wp_user_avatar')){	
+										$imgsrc = get_wp_user_avatar_src($u['user_id'],'thumbnail');				
+										if ($directorystyle==1){
+											$avatarhtml = "<img class='img img-circle alignleft' src='".$imgsrc."' width='66'  height='66' alt='".$displayname."' />";
+										}else{
+											$avatarhtml = "<img class='img alignleft' src='".$imgsrc."' width='66'  height='66' alt='".$displayname."' />";
+										}
+								} else {
+									$avatarhtml = get_avatar($u['user_id'],66,array('class'=>'alignleft'));
+									$avatarhtml = str_replace("photo", "photo alignleft", $avatarhtml);
+								}							
+								$html .= "<div class='pgrid-item'><div class='indexcard'><a href='".site_url()."/staff/".$user_info->user_nicename."/'><div class='media'>".$avatarhtml."<div class='media-body'><strong>".$displayname."</strong>".$gradedisplay."<br>";
 								// display team name(s)
 								$poduser = new Pod ('user' , $userid);
 								$terms = $poduser->get_field('user_team');
-								if ($terms) {				
-									$teamlist = array();
-							  		foreach ($terms as $taxonomy ) {
-							  			$teamlist[]= $taxonomy['name'];
-							  			$html.= implode(" &raquo; ", $teamlist)."<br>";
-			
-									}
-								}  
+								unset($poduser);
+								foreach ($terms as $taxonomy ) { //print_r($taxonomy); 
+									//echo $taxonomy['name'];
+						  		    $themeid = $taxonomy['term_id'];
+						  		    $themeparent = $taxonomy['parent']; 
+
+						  			
+						  			if ( $themeparent == 0 ) { 
+							  			$teamlist = govintranetpress_custom_title($taxonomy['name']); 
+							  			$html.= "".$teamlist."<br>"; //echo $teamlist;
+						  			} else {
+							  		    while ($themeparent!=0){
+							  		    	$newteam = get_term_by('id', $themeparent, 'team'); 
+								  			$themeparent = $newteam->parent; 
+							  		    }
+							  			$teamlist= govintranetpress_custom_title($newteam->name);
+							  			$html.= "".$teamlist."<br>";
+							  			
+						  			}
+	
+						  		}
 							
-								if ( get_user_meta($userid ,'user_job_title',true )) $html .= '<span class="small">'.get_user_meta($userid ,'user_job_title',true )."</span><br>";
+								if ( get_user_meta($userid ,'user_job_title',true )) {
+									$meta = get_user_meta($userid ,'user_job_title',true );
+									$meta = str_replace(" ", "&nbsp;", $meta);
+									$html .= '<span class="small">'.$meta."</span><br>";
+								}
 
 							if ( get_user_meta($userid ,'user_telephone',true )) $html .= '<span class="small"><i class="glyphicon glyphicon-earphone"></i> '.get_user_meta($userid ,'user_telephone',true )."</span><br>";
 							if ( get_user_meta($userid ,'user_mobile',true ) && $showmobile ) $html .= '<span class="small"><i class="glyphicon glyphicon-phone"></i> '.get_user_meta($userid ,'user_mobile',true )."</span>";
 												
-								$html .= "</div></div></div></div></a>";
+								$html .= "</div></div></a></div></div>";
+
 								
 							}																							
 						}
 						}
-						$activeletter = ($_REQUEST['show'] == strtoupper($thisletter)) ? "active" : null;
 
-						$letterlink[$thisletter] = ($hasentries[$thisletter] > 0) ? "<li  class='{$thisletter} {$activeletter}'><a href='?grade=".$grade."&show=".$thisletter."&sort={$sort}'>".$thisletter."</a></li>" : "<li class='{$thisletter} {$activeletter}'><a>".$thisletter."</a></li>";
+/* 						$letterlink[$thisletter] = ($hasentries[$thisletter] > 0) ? "<li  class='{$thisletter} {$activeletter}'><a href='?grade=".$grade."&amp;show=".$thisletter."&amp;sort={$sort}'>".$thisletter."</a></li>" : "<li class='{$thisletter} {$activeletter}'><a>".$thisletter."</a></li>"; */
 
 					}
 											echo @implode("",$letterlink); 
+															
 					?>
 					
 					</ul>
 
 					</div>
 </div>
-<div class="col-lg-4 col-md-4">
+<div class="col-lg-12 col-md-12 col-sm-12">
+<div id="sortfilter">
+<div class="col-lg-4 col-md-5 col-sm-6">
 <strong>Sort by:&nbsp;</strong>
   <?php if ($sort=="first") : ?>
   <button type="button" class="btn btn-primary">
     First name
   </button>
   <?php else : ?>
-    <a class='btn btn-default' href="<?php the_permalink(); ?>?grade=<?php echo $grade; ?>&sort=first&show=<?php echo $_REQUEST['show'] ?>">First name</a>
+    <a class='btn btn-default' href="<?php the_permalink(); ?>?grade=<?php echo $grade; ?>&amp;sort=first&amp;show=<?php echo $_REQUEST['show'] ?>">First name</a>
   <?php endif; ?>
 
   <?php if ($sort=="last") : ?>
@@ -203,31 +277,27 @@ jQuery("#s2").focus();
   	Last name
   </button>
   <?php else : ?>
-  	<a class='btn btn-default' href="<?php the_permalink(); ?>?grade=<?php echo $grade; ?>&sort=last&show=<?php echo $_REQUEST['show'] ?>">Last name</a>
+  	<a class='btn btn-default' href="<?php the_permalink(); ?>?grade=<?php echo $grade; ?>&amp;sort=last&amp;show=<?php echo $_REQUEST['show'] ?>">Last name</a>
   <?php endif; ?>
 </div>
-<div class="col-lg-8 col-md-8">
-<strong>Browse by:&nbsp;</strong>
+<div class="col-lg-8 col-md-7 col-sm-6">
+<strong>Filter by:&nbsp;</strong>
 	<div class="btn-group">
-	<?php if ($_REQUEST['grade']) : ?>
 	  <button type="button" class="btn btn-primary dropdown-toggle2" data-toggle="dropdown">
-	<?php else : ?>
-	  <button type="button" class="btn btn-default dropdown-toggle2" data-toggle="dropdown">
-	<?php endif; ?>  
 	  <?php 
-	  if ($grade){
-		  $sgrade=get_term_by( 'slug', $grade, 'grade' ) ;
-		  $sgrade=$sgrade->name;
-	  } else {
-		  $sgrade='Grade';
-	  }
-	  echo $sgrade; ?>
-	  <span class="caret"></span>
+		  if ($grade){
+			  $sgrade=get_term_by( 'slug', $grade, 'grade' ) ;
+			  $sgrade=$sgrade->name;
+		  } else {
+			  $sgrade='All grades';
+		  }
+		  echo $sgrade; ?>
+		  <span class="caret"></span>
 	  </button>
 	  <ul class="dropdown-menu" role="menu">
 		  <?php
-			$terms = get_terms('grade',array('hide_empty'=>false,'orderby'=>'slug','order'=>'ASC'));
-			echo "<li><a href='".site_url()."/staff-directory/?sort={$sort}&show=".$_REQUEST['show']."'>All grades</a></li>";
+			$terms = get_terms('grade',array('hide_empty'=>false,'orderby'=>'slug','order'=>'ASC',"parent"=>0));
+			echo "<li><a href='".site_url()."/staff-directory/?sort={$sort}&amp;show=".$_REQUEST['show']."'>All grades</a></li>";
 			if ($terms) {
 		  		foreach ((array)$terms as $taxonomy ) {
 		  		    $themeid = $taxonomy->term_id;
@@ -236,39 +306,43 @@ jQuery("#s2").focus();
 		   		    if ($themeURL == 'uncategorized') {
 			  		    continue;
 		  		    }
-		  			echo "<li><a href='".site_url()."/staff-directory/?grade={$themeURL}&sort={$sort}&show=".$_REQUEST['show']."'>".$taxonomy->name."</a></li>";
+		  			echo "<li><a href='".site_url()."/staff-directory/?grade={$themeURL}&amp;sort={$sort}&amp;show=".$_REQUEST['show']."'>".$taxonomy->name."</a></li>";
 				}
-
 			}  
-
-  ?>
+?>
 	  	</ul>
 	  	</div>
-  				<?php
-		$terms = get_terms('team',array('hide_empty'=>false,'parent' => '0',));
-		if ($terms) {
-			$otherteams="";
-			foreach ((array)$terms as $taxonomy ) {
-			    $themeid = $taxonomy->term_id;
-			    $themeURL= $taxonomy->slug;
-				$otherteams.= " <li><a href='".site_url()."/team/{$themeURL}/'>".$taxonomy->name."</a></li>";
-			}
-			echo "<div class='btn-group'><button type='button' class='btn btn-default dropdown-toggle3' data-toggle='dropdown'>Team <span class='caret'></span></button><ul class='dropdown-menu' role='menu'>".$otherteams."</ul></div>";
-		}  
-
-?>
   	</div>
 </div>
-<div class="row">
-	<div class="col-lg-12">
+</div>	
+	<div class="col-lg-12 col-md-12 col-sm-12">
   	<?php 
 	
 	$output=
-		'<div id="container" class="js-masonry"
-  data-masonry-options=\'{ "columnWidth": ".grid-sizer", "itemSelector": ".grid-item", "gutter": 10 }\'><div class="grid-sizer"></div>'.$html."</div>";
+		'<div id="container"><div class="grid-sizer"></div>'.$html."</div>";
 	echo $output;
+	echo "</div>";
 	}
 ?>
+
+<script>
+jQuery(document).ready(function($){
+var $container = jQuery('#container');
+$container.imagesLoaded(function(){
+$container.masonry({
+		itemSelector: '.pgrid-item',
+		gutter: 25,
+		isAnimated: true
+});
+});
+});
+</script>
+<div class="col-lg-12 col-md-12 col-sm-12">
+	<div class="col-lg-12 col-md-12 col-sm-12">
+		<?php the_content(); ?>
+	</div>
+</div>
+
 	</div>
 </div>
 
