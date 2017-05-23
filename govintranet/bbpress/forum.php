6cbe3c0be7fbb2b771369a3ea361e097e49a93d8
Luke Oatham <lukeoatham@gmail.com>
2017-05-23 14:29:57 +0100

    4.34.12.1 forum updates

--- govintranet/bbpress/forum.php
+++ govintranet/bbpress/forum.php
@@ -13,15 +13,14 @@ if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<div class='breadcrumbs'>
 		<?php 
 		if ( bbp_is_single_user() ):
+			$home = "<a href='" . get_home_url() . "'>" . __(get_bloginfo('name'), 'govintranet') . "</a>";
 			$sd = get_option('options_module_staff_directory_page');
 			$sd = $sd[0];
 			$directory = "<a href='" . get_permalink($sd) . "'>" . get_the_title($sd) . "</a>";
 			if ( function_exists('bcn_display') ){
 				$bcn = get_option('bcn_options');
-				$home = $bcn['Hhome_template'];
 				$sep = $bcn['hseparator'];
 			} else {
-				$home = "<a href='".site_url("/")."'>".__('Home','govintranet')."</a>";
 				$sep = " > ";
 			}
 			echo $home; 
