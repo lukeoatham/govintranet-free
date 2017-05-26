c984eb37b2a774a26e6c1187d9ea5ad65a7fe5b8
Luke Oatham <lukeoatham@gmail.com>
2017-05-26 14:40:19 +0100

    4.34.12.2
    Incorporate ACF fixes

--- govintranet/functions.php
+++ govintranet/functions.php
@@ -5137,9 +5137,11 @@ if( function_exists('acf_add_local_field_group') ){
 					'required' => 1,
 					'conditional_logic' => array (
 						array (
-							'field' => 'field_536ec2de62b52',
-							'operator' => '==',
-							'value' => '1',
+							array (
+								'field' => 'field_536ec2de62b52',
+								'operator' => '==',
+								'value' => '1',
+							),
 						),
 					),
 					'display_format' => 'd/m/Y',
@@ -5156,9 +5158,11 @@ if( function_exists('acf_add_local_field_group') ){
 					'required' => 1,
 					'conditional_logic' => array (
 						array (
-							'field' => 'field_536ec2de62b52',
-							'operator' => '==',
-							'value' => '1',
+							array (
+								'field' => 'field_536ec2de62b52',
+								'operator' => '==',
+								'value' => '1',
+							),
 						),
 					),
 					'default_value' => '',
