45758193d7fbe6dda6c69826c13b564802c600d7
lukeoatham <lukeoatham@gmail.com>
2018-04-06 23:21:59 +0100

    Feature blog posts widget
    Fixes for avatar sizing

--- plugins/ht-feature-blogposts/ht_feature_blogposts.php
+++ plugins/ht-feature-blogposts/ht_feature_blogposts.php
@@ -4,7 +4,7 @@ Plugin Name: HT Feature blogposts
 Plugin URI: https://help.govintra.net
 Description: Display blogposts
 Author: Luke Oatham
-Version: 1.5.1
+Version: 1.5.2
 Author URI: https://www.agentodigital.com
 
 */
@@ -207,11 +207,8 @@ class htFeatureBlogposts extends WP_Widget {
 					if ($thumbnails){
 						$image_uri =  wp_get_attachment_image_src( get_post_thumbnail_id( ), 'thumbnail' ); 
 						if (!$image_uri || $forceavatar){
-							$image_uri = get_avatar($post->post_author , 150, "", $user_info->display_name);
-							$image_uri = str_replace("avatar ", "avatar alignleft tinyblogthumb ".$avstyle, $image_uri);
-							$image_uri = str_replace('"150"', '"72"', $image_uri);
-							$image_uri = str_replace("'150'", "'72'", $image_uri);
-							$image_uri = str_replace("alignleft", "", $image_uri);
+							$image_uri = get_avatar($post->post_author , 72, "", $user_info->display_name, array('class'=>$avstyle));
+							$image_uri = str_replace("avatar ", "avatar ".$avstyle, $image_uri);
 							$html.= "<a class='pull-left' href='".get_permalink(get_the_id())."'>{$image_uri}</a>";		
 						} else {
 							$html.= "<a class='pull-left' href='".get_permalink(get_the_id())."'><img class='tinyblogthumb alignleft' src='{$image_uri[0]}' alt='".esc_attr($thistitle)."' /></a>";					}
@@ -281,11 +278,8 @@ class htFeatureBlogposts extends WP_Widget {
 				if ($thumbnails){
 					$image_uri =  wp_get_attachment_image_src( get_post_thumbnail_id( ), 'thumbnail' ); 
 					if (!$image_uri || $forceavatar){
-						$image_uri = get_avatar($post->post_author , 150, "", $user_info->display_name);
-						$image_uri = str_replace("avatar ", "avatar alignleft tinyblogthumb ".$avstyle, $image_uri);
-						$image_uri = str_replace('"150"', '"72"', $image_uri);
-						$image_uri = str_replace("'150'", "'72'", $image_uri);
-						$image_uri = str_replace("alignleft", "", $image_uri);
+						$image_uri = get_avatar($post->post_author , 72, "", $user_info->display_name, array('class'=>$avstyle));
+						$image_uri = str_replace("avatar ", "avatar ".$avstyle, $image_uri);
 						$html.= "<a class='pull-left' href='".get_permalink()."'>{$image_uri}</a>";		
 					} else {
 						$html.= "<a class='pull-left' href='".get_permalink()."'><img class='tinyblogthumb alignleft' src='{$image_uri[0]}' alt='".esc_attr($thistitle)."' /></a>";						
